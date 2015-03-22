ef618fd8008fdcee8ba82ba56bd5160d662e3568
Nikhil Navali <nikhilnavali@gmail.com>
2015-03-22 19:04:13 -0400

    Added code to pick only the UP servers

--- SessionManager/src/com/scheduler/ViewExchangerThread.java
+++ SessionManager/src/com/scheduler/ViewExchangerThread.java
@@ -2,25 +2,39 @@ package com.scheduler;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Map.Entry;
+
 import com.servlet.EnterServlet;
 import com.util.SessionUtil;
+import com.view.ServerStatus;
 import com.view.ViewUtils;
+import com.view.ServerStatus.ServerStatusCode;
 
 public class ViewExchangerThread implements Runnable{
 	@Override
 	public void run() {
 		String gossipPartnerIp = pickGossipPartner();
 		if(gossipPartnerIp.equals(SessionUtil.getIpAddress())) {
-			//exchange with simple db
+			//Exchange with SimpleDB
 			ViewUtils.exchangeViewWithSimpleDb();
 		} else {
-			//make rpc and exchange with server
+			//Make RPC and exchange with server
 		}
 	}
 	
 	private static String pickGossipPartner() {
-		//TODO Pick a partner from only the UP servers
-		ArrayList<String> serverIdList = new ArrayList<String>(EnterServlet.myView.keySet());
+		//Pick a partner from only the servers which are running
+		ArrayList<String> serverIdList = new ArrayList<String>();
+		
+		//List only those servers which are running
+		for (Entry<String, ServerStatus> serverEntry : EnterServlet.myView.entrySet()) {
+			String serverID = serverEntry.getKey();
+			ServerStatus serverStatus = serverEntry.getValue();
+			if(serverStatus.getStatus().equals(ServerStatusCode.UP)) {
+				serverIdList.add(serverID);
+			}
+				
+		}
 		Random rand = new Random();
 		return serverIdList.get(rand.nextInt(serverIdList.size()));
 	}
