f25a7aec5a97ebb590990127a90495767e1cf7c7
Nikhil Navali <nikhilnavali@gmail.com>
2015-03-23 00:21:10 -0400

    Server program updated with code for exchange views

--- SessionManager/src/com/rpc/SMRPCServer.java
+++ SessionManager/src/com/rpc/SMRPCServer.java
@@ -5,51 +5,66 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.util.HashMap;
 
 import com.servlet.EnterServlet;
-
+import com.view.ServerStatus;
+import com.view.ServerStatus.ServerStatusCode;
 
 public class SMRPCServer implements Runnable {
 	private static final int portPROJ1BRPC = 5300;
 	private static final int packetSize = 512;
 	private static final String NOT_FOUND = "not found";
 	private static final String SESSION_WRITE_SUCCESS = "session written successfully";
-	
+	private static final String EXCHANGE_VIEW_SUCCESS = "views exchanged succesfully";
+
 	@Override
 	public void run() {
 		try {
 			DatagramSocket rpcSocket = new DatagramSocket(portPROJ1BRPC);
-			while(true) {
+			while (true) {
 				byte[] inBuf = new byte[packetSize];
-			    DatagramPacket recvPkt = new DatagramPacket(inBuf, inBuf.length);
+				DatagramPacket recvPkt = new DatagramPacket(inBuf, inBuf.length);
+				// Waits till the packet is received
 				rpcSocket.receive(recvPkt);
 				InetAddress returnAddr = recvPkt.getAddress();
 				int returnPort = recvPkt.getPort();
 				// here inBuf contains the callID and operationCode
-				String receivedRPCPktStr = new String(recvPkt.getData(), 0, recvPkt.getLength());
-				RPCRequest rpcRequest = RPCMsgUtil.deserializeRPCRequest(receivedRPCPktStr);
+				String receivedRPCPktStr = new String(recvPkt.getData(), 0,
+						recvPkt.getLength());
+				RPCRequest rpcRequest = RPCMsgUtil
+						.deserializeRPCRequest(receivedRPCPktStr);
 				int operationCode = rpcRequest.getOpCode();
 				byte[] outBuf = null;
 				String payLoad = "";
 				RPCResponse rpcResponse = null;
-				switch(operationCode) {
-					   case 1:
-						   payLoad = getPayloadForSessionRead(rpcRequest.getPayload()); //rpcRequest will have session id in payload
-						   rpcResponse = new RPCResponse(rpcRequest.getCallId(), payLoad);
-						   break;
-					   case 2:
-						   payLoad = getPayloadForSessionWrite(rpcRequest.getPayload()); //write session id
-						   rpcResponse = new RPCResponse(rpcRequest.getCallId(), payLoad);
-						   break;
-					   case 3:
-						   payLoad = "view merged successfully"; //exchange view
-						   rpcResponse = new RPCResponse(rpcRequest.getCallId(), payLoad);
-						   break;
+				switch (operationCode) {
+				case 1:
+					// rpcRequest will contain sessionID in payload
+					payLoad = getPayloadForSessionRead(rpcRequest.getPayload());
+					rpcResponse = new RPCResponse(rpcRequest.getCallId(),
+							payLoad);
+					break;
+				case 2:
+					// rpcRequest will contain session Data to be written
+					payLoad = getPayloadForSessionWrite(rpcRequest.getPayload());
+					rpcResponse = new RPCResponse(rpcRequest.getCallId(),
+							payLoad);
+					break;
+				case 3:
+					// rpcRequest will contain foreign server's view to be
+					// exchanged
+					payLoad = getPayloadForExchangeView(rpcRequest.getPayload());
+					rpcResponse = new RPCResponse(rpcRequest.getCallId(),
+							payLoad);
+					break;
 				}
-				outBuf = RPCMsgUtil.serializeRPCResponse(rpcResponse).getBytes();
-				//here outBuf should contain the callID and results of the call
-				DatagramPacket sendPkt = new DatagramPacket(outBuf, outBuf.length,returnAddr, returnPort);
-			    rpcSocket.send(sendPkt);
+				outBuf = RPCMsgUtil.serializeRPCResponse(rpcResponse)
+						.getBytes();
+				// here outBuf should contain the callID and results of the call
+				DatagramPacket sendPkt = new DatagramPacket(outBuf,
+						outBuf.length, returnAddr, returnPort);
+				rpcSocket.send(sendPkt);
 			}
 		} catch (SocketException e) {
 			e.printStackTrace();
@@ -57,34 +72,59 @@ public class SMRPCServer implements Runnable {
 			e.printStackTrace();
 		}
 	}
-	
+
+	/*
+	 * Check if the sessionID is present in the local session table
+	 * @return <versionNumber,sessionData> OR Not found
+	 */
 	private static String getPayloadForSessionRead(String incomingPayload) {
-	    String sessionId = incomingPayload;
-	    String output = EnterServlet.sessionTable.get(sessionId);
-	    if(null != output) {
-	    	String[] tokens = output.split("_");
-	    	return tokens[1] + "," + output;
-	    } else {
-	    	return NOT_FOUND;
-	    }
+		String sessionId = incomingPayload;
+		String sessionData = EnterServlet.sessionTable.get(sessionId);
+		if (null != sessionData) {
+			String[] tokens = sessionData.split("_");
+			return tokens[1] + "," + sessionData;
+		} else {
+			return NOT_FOUND;
+		}
 	}
-	
+
+	/*
+	 * Write the session data to local session table
+	 * @return Success message 
+	 */
 	private static String getPayloadForSessionWrite(String incomingPayload) {
 		String[] tokens = incomingPayload.split("_");
 		String sessionId = tokens[0];
-		String versionNumber = tokens[1];
+		//String versionNumber = tokens[1];
 		String data = tokens[2];
-		String timeStamp = tokens[3];
-		
+		//String timeStamp = tokens[3];
+
 		EnterServlet.sessionTable.put(sessionId, data);
-			return SESSION_WRITE_SUCCESS;	
+		return SESSION_WRITE_SUCCESS;
 	}
-	
-	//Get all the server triplets and exchange the views
-	private static String getPayloadForExchangeView() {
-		return "";
+
+	/*
+	 * Get all the server triplets and exchange the views
+	 * @return Success message 
+	 */
+	private static String getPayloadForExchangeView(String incomingPayload) {
+
+		if (incomingPayload != null) {
+			// Prepare the view with which local view has to be exchanged
+			HashMap<String, ServerStatus> hisView = new HashMap<String, ServerStatus>();
+			String[] triplets = incomingPayload.split(";");
+			for (String serverTriplet : triplets) {
+				String serverDetails[] = serverTriplet.split("_");
+				String serverID = serverDetails[0];
+				ServerStatus status = new ServerStatus(
+						ServerStatusCode.valueOf(serverDetails[1]));
+				status.setTime(Long.parseLong(serverDetails[2]));
+				hisView.put(serverID, status);
+			}
+
+		}
+
+		return EXCHANGE_VIEW_SUCCESS;
 	}
-	
+
 }
-
-
