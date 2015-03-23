a3674e296d580f95e6848d47c8ce317cf6997e98
Nikhil Navali <nikhilnavali@gmail.com>
2015-03-23 19:57:06 -0400

    Added code to intitialize view with its own triple

--- SessionManager/src/com/scheduler/ViewExchangerThread.java
+++ SessionManager/src/com/scheduler/ViewExchangerThread.java
@@ -1,20 +1,26 @@
 package com.scheduler;
 
 import java.util.ArrayList;
-import java.util.Random;
 import java.util.Map.Entry;
+import java.util.Random;
 
 import com.servlet.EnterServlet;
 import com.util.SessionUtil;
 import com.view.ServerStatus;
-import com.view.ViewUtils;
 import com.view.ServerStatus.ServerStatusCode;
+import com.view.ViewUtils;
 
-public class ViewExchangerThread implements Runnable{
+public class ViewExchangerThread implements Runnable {
 	@Override
 	public void run() {
 		String gossipPartnerIp = pickGossipPartner();
-		if(gossipPartnerIp.equals(SessionUtil.getIpAddress())) {
+		
+		String localsvrId = SessionUtil.getIpAddress();
+		
+		//Update its own tuple
+		EnterServlet.myView.put(localsvrId, new ServerStatus(ServerStatusCode.UP));
+		
+		if(gossipPartnerIp.equals(localsvrId)) {
 			//Exchange with SimpleDB
 			ViewUtils.exchangeViewWithSimpleDb();
 		} else {
@@ -22,19 +28,20 @@ public class ViewExchangerThread implements Runnable{
 			ViewUtils.exchangeViewWithServer(gossipPartnerIp);
 		}
 	}
-	
+
 	private static String pickGossipPartner() {
-		//Pick a partner from only the servers which are running
+		// Pick a partner from only the servers which are running
 		ArrayList<String> serverIdList = new ArrayList<String>();
-		
-		//List only those servers which are running
-		for (Entry<String, ServerStatus> serverEntry : EnterServlet.myView.entrySet()) {
+
+		// List only those servers which are running
+		for (Entry<String, ServerStatus> serverEntry : EnterServlet.myView
+				.entrySet()) {
 			String serverID = serverEntry.getKey();
 			ServerStatus serverStatus = serverEntry.getValue();
-			if(serverStatus.getStatus().equals(ServerStatusCode.UP)) {
+			if (serverStatus.getStatus().equals(ServerStatusCode.UP)) {
 				serverIdList.add(serverID);
 			}
-				
+
 		}
 		Random rand = new Random();
 		return serverIdList.get(rand.nextInt(serverIdList.size()));
