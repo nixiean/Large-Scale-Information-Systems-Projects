66d554ac2a0c8fc3e5b1039f5f703544c17ba483
Nikhil Navali <nikhilnavali@gmail.com>
2015-05-03 17:00:07 -0400

    Comments

--- PageRankInHadoop/src/PageRank.java
+++ PageRankInHadoop/src/PageRank.java
@@ -26,6 +26,9 @@ public class PageRank {
 	static long rejectCount = 0;
 	static long acceptCount = 0;
 
+	/*
+	 * Read the input edges file and store it in the local data structure 
+	 */
 	public static void processInputFile(String inputFile)
 			throws FileNotFoundException, IOException {
 		try (BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
@@ -36,12 +39,15 @@ public class PageRank {
 				long startNode = Long.valueOf(tokens[2]);
 				long endNode = Long.valueOf(tokens[3]);
 				String nodeValue = null;
+				//Get the adjacency list for a node which is present in the data structure
 				if (adjacencyList.containsKey(startNode))
 					nodeValue = adjacencyList.get(startNode);
 
+				//Insert the node if it is not present in the data structure
 				if (!adjacencyList.containsKey(endNode))
 					adjacencyList.put(endNode, null);
 
+				//Check if the edge has to be selected and update the adjacency list for the node
 				if (isSelectEdge(selectValue)) {
 					acceptCount++;
 					if (nodeValue != null)
@@ -51,18 +57,30 @@ public class PageRank {
 						nodeValue = String.valueOf(endNode);
 				} else
 					rejectCount++;
-
+				
+				//Update the data structure
 				adjacencyList.put(startNode, nodeValue);
 			}
 		}
+		System.out.println("*******************************************************");
+		System.out.println("Total number of nodes = " + adjacencyList.size());
+		System.out.println("Total number of edges = " + acceptCount + rejectCount);
 		System.out.println("Accept Count = " + acceptCount);
 		System.out.println("Reject Count = " + rejectCount);
+		System.out.println("Percentage reject = " + ((rejectCount)/(rejectCount + acceptCount)));
+		System.out.println("*******************************************************");
 	}
 
+	/*
+	 * Check if the edge has to be selected
+	 */
 	public static boolean isSelectEdge(double x) {
 		return (((x >= rejectMin) && (x < rejectLimit)) ? false : true);
 	}
 
+	/*
+	 * Write the local data structure to a file which is suitable for Map Reduce
+	 */
 	public static void writeToFile(String fOut) throws IOException {
 
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(
@@ -71,11 +89,13 @@ public class PageRank {
 		for (Entry<Long, String> entry : adjacencyList.entrySet()) {
 			String value = entry.getValue();
 			Long key = entry.getKey();
+			//If there are outgoing edges
 			if (value != null)
 				bw.write(String.valueOf(key) + tokenDelimiter
 						+ String.valueOf(pageRank) + tokenDelimiter
 						+ String.valueOf(value.split(",").length) + tokenDelimiter + value);
 			else
+				//If there are outgoing edges
 				bw.write(String.valueOf(key) + tokenDelimiter
 						+ String.valueOf(pageRank) + tokenDelimiter
 						+ 0 + tokenDelimiter);
@@ -87,7 +107,9 @@ public class PageRank {
 
 	public static void main(String[] args) throws FileNotFoundException,
 			IOException {
+		//Read and process input file
 		processInputFile(inputFile);
+		//Write to output file
 		writeToFile(outputFile);
 	}
 }
